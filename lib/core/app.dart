beba2a83df5204cd7aba0c6202a3b37c12d83cbd
Toh Chen Long <chenlongtoh@gmail.com>
2024-04-13 07:46:34 +0800

    Initial commit

---
+++ lib/core/app.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/core/home/home_page.dart';
+import 'package:movie_app/core/search/search_page.dart';
+
+const List<Widget> _kScreens = [HomePage(), SearchPage()];
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int _index = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _onTap(int index) => setState(() => _index = index);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _index,
+        children: _kScreens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onTap,
+        currentIndex: _index,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.white,
+        selectedIconTheme: const IconThemeData(size: 30),
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        enableFeedback: true,
+        showUnselectedLabels: true,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "Search",
+          ),
+        ],
+      ),
+    );
+  }
+}
