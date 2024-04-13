286d6721502953279378e9ff5fd87e47a31e4563
Toh Chen Long <chenlongtoh@gmail.com>
2024-04-13 23:04:32 +0800

    Add common text style

---
+++ lib/theme/text_style.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MovieAppTextStyle {
+  // Light
+  static const light10 = TextStyle(fontSize: 10, fontWeight: FontWeight.w300);
+  static const light12 = TextStyle(fontSize: 12, fontWeight: FontWeight.w300);
+  static const light14 = TextStyle(fontSize: 14, fontWeight: FontWeight.w300);
+  static const light16 = TextStyle(fontSize: 16, fontWeight: FontWeight.w300);
+
+  // Regular
+  static const regular10 = TextStyle(fontSize: 10, fontWeight: FontWeight.w400);
+  static const regular12 = TextStyle(fontSize: 12, fontWeight: FontWeight.w400);
+  static const regular14 = TextStyle(fontSize: 14, fontWeight: FontWeight.w400);
+  static const regular16 = TextStyle(fontSize: 16, fontWeight: FontWeight.w400);
+
+  // Medium
+  static const medium10 = TextStyle(fontSize: 10, fontWeight: FontWeight.w600);
+  static const medium12 = TextStyle(fontSize: 12, fontWeight: FontWeight.w600);
+  static const medium14 = TextStyle(fontSize: 14, fontWeight: FontWeight.w600);
+  static const medium16 = TextStyle(fontSize: 16, fontWeight: FontWeight.w600);
+
+  // Regular
+  static const semibold10 = TextStyle(fontSize: 10, fontWeight: FontWeight.w600);
+  static const semibold12 = TextStyle(fontSize: 12, fontWeight: FontWeight.w600);
+  static const semibold14 = TextStyle(fontSize: 14, fontWeight: FontWeight.w600);
+  static const semibold16 = TextStyle(fontSize: 16, fontWeight: FontWeight.w600);
+
+  // Medium
+  static const bold10 = TextStyle(fontSize: 10, fontWeight: FontWeight.w700);
+  static const bold12 = TextStyle(fontSize: 12, fontWeight: FontWeight.w700);
+  static const bold14 = TextStyle(fontSize: 14, fontWeight: FontWeight.w700);
+  static const bold16 = TextStyle(fontSize: 16, fontWeight: FontWeight.w700);
+}
